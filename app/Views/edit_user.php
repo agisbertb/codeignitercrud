7eb911d81036d0eef2f64f1769df1f0f923f4f3d
Andreu Gisbert Bel <91249151+agisbertb@users.noreply.github.com>
2024-10-07 17:20:43 +0200

    Fix link on image on home, edit and user pages

--- app/Views/edit_user.php
+++ app/Views/edit_user.php
@@ -37,7 +37,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-dark bg-dark py-3">
         <div class="container d-flex justify-content-center">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="/">
                 <img src="<?= base_url('assets/images/Logo.webp') ?>" alt="CodeIgniter Logo" class="img-fluid" style="max-height: 99px;">
             </a>
         </div>
