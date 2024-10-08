053ad90adeaac9b6faec9d0d8119b499e051d9be
Andreu Gisbert Bel <91249151+agisbertb@users.noreply.github.com>
2024-10-08 21:14:27 +0200

    Fix edit and delete buttons on home with mode responsive

--- app/Views/home.php
+++ app/Views/home.php
@@ -75,8 +75,8 @@
                                         <td><?= $user['email'] ?></td>
                                         <td><?= $user['phone'] ?></td>
                                         <td class="text-center">
-                                            <div class="d-flex flex-column">
-                                                <a href="/edit/<?= $user['id'] ?>" class="btn btn-warning btn-sm mb-1">Edit</a>
+                                            <div class="d-flex d-md-inline-flex flex-column flex-md-row">
+                                                <a href="/edit/<?= $user['id'] ?>" class="btn btn-warning btn-sm mb-1 mb-md-0 me-md-1">Edit</a>
                                                 <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal" data-bs-id="<?= $user['id'] ?>">Delete</button>
                                             </div>
                                         </td>
